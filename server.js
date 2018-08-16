3100d00b36fef63be58719632bfe8a8c5d84b055
Daemone <kylebehm@gmail.com>
2018-08-16 18:32:20 -0400

    server-complete

--- server.js
+++ server.js
@@ -38,69 +38,95 @@ mongoose.connect(MONGODB_URI);
 // ROUTES
 
 // Get route for scraping CoinTelegraph. Only run this once.
-app.get("/scrape", function(req, res) {
-  // Grab the HTML body using request
-  axios.get("http://www.cointelegraph.com/").then(function(response) {
-    // $ is shorthand for HTML body
-    var $ = cheerio.load(response.data);
+app.get("/scrape", function (req, res) {
+    // Grab the HTML body using request
+    axios.get("http://www.cointelegraph.com/").then(function (response) {
+        // $ is shorthand for HTML body
+        var $ = cheerio.load(response.data);
 
-    // Now, we grab every image within a post tag, and do the following:
-    $(".post .image a").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
+        // Now, we grab every image within a post tag, and do the following:
+        $(".post .image a").each(function (i, element) {
+            // Save an empty result object
+            var result = {};
 
-      // Add the alt-text, text, and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("img")
-        .attr("alt");
-      result.summary = $(this)
-        .children(".lead")
-        .text();
-      result.link = $(this)
-        // .children("a")
-        .attr("href");
+            // Add the alt-text, text, and href of every link, and save them as properties of the result object
+            result.title = $(this)
+                .children("img")
+                .attr("alt");
+            result.summary = $(this)
+                .children(".lead")
+                .text();
+            result.link = $(this)
+                .attr("href");
 
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, send it to the client
-          return res.json(err);
+            // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    // If an error occurred, send it to the client
+                    return res.json(err);
+                });
         });
+
+        // If we were able to successfully scrape and save an Article, send a message to the client
+        res.send("Scrape Complete");
     });
-
-    // If we were able to successfully scrape and save an Article, send a message to the client
-    res.send("Scrape Complete");
-  });
 });
 
-// Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-  // TODO: Finish the route so it grabs all of the articles
+// HTML ROUTES
+
+// Route for all articles
+app.get("/articles", function (req, res) {
+    // find query with no filter to find all
+    db.Article.find({})
+        .then(function (dbArticle) {
+            // send json of article document
+            res.json(dbArticle);
+        })
+        // error catch
+        .catch(function (err) {
+            res.json(err);
+        });
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-  // TODO
-  // ====
-  // Finish the route so it finds one article using the req.params.id,
-  // and run the populate method with "note",
-  // then responds with the article with the note included
+// Route for one specific article
+app.get("/articles/:id", function (req, res) {
+    // use req.params and a find query with a filter on id
+    db.Article.findOne({ _id: req.params.id })
+        // populate method allows user to see specific details (properties) of the note
+        .populate("note")
+        .then(function (dbArticle) {
+            // if found send the json
+            res.json(dbArticle);
+        })
+        // error catch
+        .catch(function (err) {
+            res.json(err);
+        });
 });
 
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // TODO
-  // ====
-  // save the new note that gets posted to the Notes collection
-  // then find an article from the req.params.id
-  // and update it's "note" property with the _id of the new note
+// Post route for updating article note
+app.post("/articles/:id", function (req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            // user findOneAndUpdate method with two filters. specify true to return the modified document.
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then(function (dbArticle) {
+            // if succesful send the JSON
+            res.json(dbArticle);
+        })
+        // error catch
+        .catch(function (err) {
+            res.json(err);
+        });
 });
 
 // Start the server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
 });
